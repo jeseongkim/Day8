ab665725df164ce36eb0a1ffe438fcb2a023c595
Jinchan <ghkdwlscks@gmail.com>
2025-07-02 10:02:57 +0900

    [feature] 48

--- Project1/prime-factors_test.cpp
+++ Project1/prime-factors_test.cpp
@@ -17,3 +17,8 @@ TEST_F(PrimeFixture, Of2) {
 	vector<int> expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
+
+TEST_F(PrimeFixture, Of3) {
+	vector<int> expected = { 3 };
+	EXPECT_EQ(expected, prime_factor.of(3));
+}
